064523cabe2b33a947bc6d89f231af374458732f
Jonathan Belcher <belcherj@gmail.com>
2012-05-25 08:27:37 -0400

    no message

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+<?php 
+
+   $app_id = "390782660963770";
+   $app_secret = "378cc6340c90c1e0b4b10c498ff66120";
+   $my_url = "https://high-leaf-9955.herokuapp.com/";
+
+   session_start();
+   $code = $_REQUEST["code"];
+
+   if(empty($code)) {
+     $_SESSION['state'] = md5(uniqid(rand(), TRUE)); //CSRF protection
+     $dialog_url = "http://www.facebook.com/dialog/oauth?client_id=" 
+       . $app_id . "&redirect_uri=" . urlencode($my_url) . "&state="
+       . $_SESSION['state'];
+
+     echo("<script> top.location.href='" . $dialog_url . "'</script>");
+   }
+
+   if($_REQUEST['state'] == $_SESSION['state']) {
+     $token_url = "https://graph.facebook.com/oauth/access_token?"
+       . "client_id=" . $app_id . "&redirect_uri=" . urlencode($my_url)
+       . "&client_secret=" . $app_secret . "&code=" . $code;
+
+     $response = file_get_contents($token_url);
+     $params = null;
+     parse_str($response, $params);
+
+     $graph_url = "https://graph.facebook.com/me?access_token=" 
+       . $params['access_token'];
+
+     $user = json_decode(file_get_contents($graph_url));
+     echo("Hello " . $user->name);
+   }
+   else {
+     echo("The state does not match. You may be a victim of CSRF.");
+   }
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
